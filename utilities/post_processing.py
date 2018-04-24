393f1351c3254a423af3437122847d75fc78060a
yolanda93h <yolanda93h@gmail.com>
2018-04-24 23:03:23 +0200

    modified subsampling function

--- utilities/post_processing.py
+++ utilities/post_processing.py
@@ -1,35 +1,46 @@
 # some functionalities to process the datasets
 
 from pyspark.sql import SQLContext
-from pyspark.sql.functions import rank, min, dense_rank, percent_rank, ntile, col
+from pyspark.sql import functions
+from pyspark.sql.functions import rank, min, dense_rank, percent_rank, ntile, col, row_number
 from pyspark.sql.window import Window
-import statistics
+from utilities import statistics, post_processing
 import csv
 
-def subsampling(df, ranges=None, bins=None, sample_ratios=None, take_first=None):
+def subsampling(spark_ctx, sql_context, df, ranges=None, bins=None, sample_ratios=None, take_first=None):
 	"""
 	tcp_complete datasets
 	range is an array of tuples
 	"""
-	connect_pkts = statistics.get_total_pkts_flowid(df)
 
-	df_ranked = statistics.compute_ranking(connect_pkts, ranges, bins)
+	df_ranked, rank_counts = statistics.compute_ranking(df, ranges, bins)
 
-	subsampled = sqlContext.createDataFrame(sc.emptyRDD(), df_ranked.schema)
-	
+	subsampled = sql_context.createDataFrame(spark_ctx.emptyRDD(), df_ranked.schema)
+
+	ranks = len(ranges) if ranges else bins
+
 	if sample_ratios:
 
-		for i, ratio in zip(range(len(ranges)),sample_ratios):
-				subsampled = subsampled.union(df_ranked.filter(df_ranked['rank']==i).sample(False, round(ratio), seed=0))
+		for i, ratio in zip(range(ranks),sample_ratios):
+				subsampled = subsampled.union(df_ranked.filter(df_ranked['rank']==i+1).sample(False, float(ratio), seed=0))
 
 	elif take_first:
 
-		for i, ntake in zip(range(len(ranges)),take_first):
-			if ntake == 'n':
-				ratio = df_ranked.count()
-			else:
-				ratio = round(ntake / df_ranked.count())
+		for i, ntake in zip(range(ranks),take_first):
 
-		subsampled = subsampled.union(df_ranked.filter(df_ranked['rank']==i).sample(False, round(ratio), seed=0))
+			df_ranked_filtered = df_ranked.filter(df_ranked['rank']==i+1)
+
+			if ntake == 'n':
+				subsampled = subsampled.union(df_ranked_filtered)
+
+			else:
+				window = Window.partitionBy("Flow_ID").orderBy(col("first:29").desc())
+
+				subsampled = subsampled.union(df_ranked_filtered.withColumn("r", functions.row_number().over(window)).where(col("r") <= ntake).drop('r'))
+			#else:
+			#	nflows = df_ranked_filtered.select("Flow_ID").distinct().rdd.map(lambda r: r[0]).collect()
+			#	for flow_id in nflows:
+			#		df_ranked_flow_filtered = df_ranked_filtered.filter(df_ranked_filtered['Flow_ID']==flow_id)
+			#		subsampled = subsampled.union(df_ranked_flow_filtered.sample(False, float(ratio), seed=0))
 
 	return subsampled
--- utilities/statistics.py
+++ utilities/statistics.py
@@ -1,6 +1,7 @@
 # some functionalities to compute statistics
 
 from pyspark.sql import SQLContext
+from pyspark.sql import functions
 from pyspark.sql.functions import rank, min, dense_rank, percent_rank, ntile, col
 from pyspark.sql.window import Window
 import csv
@@ -11,27 +12,33 @@ def get_total_pkts_flowid(df):
 	connect_pkts =  df.groupby(['#c_ip:1','c_port:2','s_ip:15','s_port:16','tag:132']).agg({'c_pkts_all:3':'sum','s_pkts_all:17':'sum'})
 
 	# Get npackets per flow
-	connect_pkts = connect_pkts.withColumn('total_pkts',connect_pkts['sum(c_pkts_all:3)'].__add__(connect_pkts['sum(s_pkts_all:17)']))
+	connect_total_pkts = connect_pkts.withColumn('total_pkts',connect_pkts['sum(c_pkts_all:3)'].__add__(connect_pkts['sum(s_pkts_all:17)']))
 
-	return connect_pkts
+	connect_total_pkts_flow = connect_total_pkts.withColumn("Flow_ID",functions.monotonically_increasing_id()) # assign the flow_id to each flow belonging to the same connection
+
+	return connect_total_pkts_flow
 
 
 def compute_ranking(df, ranges=None, bins=None):
 
-	connect_pkts = get_total_pkts_flowid(df)
+	df_cnt_pkts = get_total_pkts_flowid(df)
+	rank_count = None
 
 	if bins:
 	   wSpec3 = Window.orderBy('total_pkts')
-	   connect_ranks = connect_pkts.withColumn("rank", ntile(bins).over(wSpec3))
+	   connect_ranks = df_cnt_pkts.withColumn("rank", ntile(bins).over(wSpec3))
 	   rank_count = connect_ranks.groupby('rank').agg({'rank':'count'})
 
 	if ranges:
 		rank = 1
 		for range in ranges: 
-		 	connect_ranks = connect_ranks.withColumn('rank',when( (connect_ranks['total_pkts'] >= range[0]) & (connect_ranks['total_pkts'] <= range[1]) , rank).otherwise(-1))
-		 	rank += 1
+
+			range_1 = df_cnt_pkts.count() if range[1] == 'n' else range[1]
+
+			df_cnt_pkts = df_cnt_pkts.withColumn('rank',functions.when( (df_cnt_pkts['total_pkts'] >= range[0]) & (df_cnt_pkts['total_pkts'] <= range_1) , rank).otherwise(rank-1))
+			rank += 1
 
 	# add the rank to the subflows/rows of the original dataset
-	df_rank = df.alias('a').join(connect_ranks.alias('b'), (col('a.#c_ip:1') == col('b.#c_ip:1')) & (col('a.c_port:2') == col('b.c_port:2')) & (col('a.s_ip:15') == col('b.s_ip:15')) & (col('a.s_port:16') == col('b.s_port:16')) & (col('a.tag:132') == col('b.tag:132')), how="left").select([col('a.'+ xx) for xx in df.columns] + [col('b.rank')])
+	df_rank = df.alias('a').join(df_cnt_pkts.alias('b'), (col('a.#c_ip:1') == col('b.#c_ip:1')) & (col('a.c_port:2') == col('b.c_port:2')) & (col('a.s_ip:15') == col('b.s_ip:15')) & (col('a.s_port:16') == col('b.s_port:16')) & (col('a.tag:132') == col('b.tag:132')), how="left").select([col('a.'+ xx) for xx in df.columns] + [col('b.rank'),col('b.Flow_ID')] )
 
 	return df_rank, rank_count
